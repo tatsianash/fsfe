f30ea348a7dcce154bcedb291cc27ce1c0986c50
Tatsiana Sh <tatsiana15az@gmail.com>
2024-03-02 18:55:34 -0700

    update ws

--- index-ws.js
+++ index-ws.js
@@ -1,43 +1,51 @@
-const express = require("express");
-const server = require("http").createServer();
+const express = require('express');
+const server = require('http').createServer();
 const app = express();
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: __dirname});
+});
 
+server.on('request', app);
 
-server.on("request", app);
-server.listen(3000, () => console.log("server started on port 3000"));
+server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+/** Websocket **/
+const WebSocketServer = require('ws').Server;
 
-/* begin websocket */
+const wss = new WebSocketServer({ server: server });
 
-const WebSocketServer = require("ws").Server;
-const wss = new WebSocketServer({ server });
-
-wss.on('connection', function connection(ws){
+wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size;
-  console.log("Client connected", numClients);
-  wss.broadcast(`Current number of visitors: ${numClients}`);
 
-  if(ws.readyState === ws.OPEN) {
-    ws.send('Hello, client!');
+  console.log('clients connected: ', numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('welcome!');
   }
 
   ws.on('close', function close() {
-    console.log("Client disconnected", numClients);
-    wss.broadcast(`Current number of visitors: ${numClients}`);
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log('A client has disconnected');
   });
 
+  ws.on('error', function error() {
+    //
+  });
 });
 
-
-wss.broadcast = function broadcast(msg) {
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log('Broadcasting: ', data);
   wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocketServer.OPEN) {
-      client.send(msg);
-    }
+    client.send(data);
   });
 };
-
+/** End Websocket **/
